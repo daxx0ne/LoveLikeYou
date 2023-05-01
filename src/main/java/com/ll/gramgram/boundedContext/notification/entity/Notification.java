4291e3c8371e87dcdff97419293d1300d0badd60
daone <jcn07253@gmail.com>
2023-05-01 10:13:15 +0900

    웹폰트 변경, 알림리스트 디자인

---
+++ src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
@@ -0,0 +1,32 @@
+package com.ll.gramgram.boundedContext.notification.entity;
+
+import com.ll.gramgram.base.baseEntity.BaseEntity;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Notification extends BaseEntity {
+    private LocalDateTime readDate;
+    @ManyToOne
+    @ToString.Exclude
+    private InstaMember toInstaMember; // 메세지 받는 사람(호감 받는 사람)
+    @ManyToOne
+    @ToString.Exclude
+    private InstaMember fromInstaMember; // 메세지를 발생시킨 행위를 한 사람(호감표시한 사람)
+    private String typeCode; // 호감표시=Like, 호감사유변경=ModifyAttractiveType
+    private String oldGender; // 해당사항 없으면 null
+    private int oldAttractiveTypeCode; // 해당사항 없으면 0
+    private String newGender; // 해당사항 없으면 null
+    private int newAttractiveTypeCode; // 해당사항 없으면 0
+}
