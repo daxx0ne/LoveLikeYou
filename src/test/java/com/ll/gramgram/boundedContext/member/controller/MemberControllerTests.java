af9e6d1213f3a5b6706dc718d8ed1ce5184f2350
daone <jcn07253@gmail.com>
2023-05-01 10:00:19 +0900

    쓸데없는 기능 삭제

--- src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
@@ -1,8 +1,6 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
 
-import com.ll.gramgram.boundedContext.home.controller.HomeController;
-import com.ll.gramgram.boundedContext.member.entity.Member;
 import com.ll.gramgram.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.DisplayName;
@@ -12,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -38,122 +35,11 @@ public class MemberControllerTests {
     private MemberService memberService;
 
     @Test
-    @DisplayName("회원가입 폼")
-    void t001() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get("/usr/member/join"))
-                .andDo(print()); // 크게 의미 없고, 그냥 확인용
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("showJoin"))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(containsString("""
-                        <input type="text" name="username"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        <input type="password" name="password"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        id="btn-join-1"
-                        """.stripIndent().trim())));
-    }
-
-    @Test
-    // @Rollback(value = false) // DB에 흔적이 남는다.
-    @DisplayName("회원가입")
-    void t002() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10")
-                        .param("password", "1234")
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrlPattern("/usr/member/login?msg=**"));
-
-        Member member = memberService.findByUsername("user10").orElse(null);
-
-        assertThat(member).isNotNull();
-    }
-
-    @Test
-    @DisplayName("회원가입시에 올바른 데이터를 넘기지 않으면 400")
-    void t003() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10")
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-
-        // WHEN
-        resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("password", "1234")
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-
-        // WHEN
-        resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10" + "a".repeat(30))
-                        .param("password", "1234")
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-
-        // WHEN
-        resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10")
-                        .param("password", "1234" + "a".repeat(30))
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-    }
-
-    @Test
     @DisplayName("로그인 폼")
     void t004() throws Exception {
         // WHEN
         ResultActions resultActions = mvc
-                .perform(get("/usr/member/login"))
+                .perform(get("/usr/member/login?admin"))
                 .andDo(print());
 
         // THEN
@@ -199,24 +85,4 @@ public class MemberControllerTests {
                 .andExpect(redirectedUrlPattern("/**"));
     }
 
-    @Test
-    // @Rollback(value = false) // DB에 흔적이 남는다.
-    @DisplayName("로그인 후에 내비바에 로그인한 회원의 username")
-    @WithUserDetails("user1")
-        // user1로 로그인 한 상태로 진행
-    void t006() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get("/"))
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(HomeController.class))
-                .andExpect(handler().methodName("showMain"))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(containsString("""
-                        0002
-                        """.stripIndent().trim())));
-    }
 }
