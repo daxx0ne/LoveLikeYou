04ed26441f4b3b65021893c71cdd6d6b6162933b
daone <jcn07253@gmail.com>
2023-05-01 10:13:47 +0900

    쓸데 없는 ExceptionHandler 함수 제거

--- src/main/java/com/ll/gramgram/base/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/ll/gramgram/base/exceptionHandler/GlobalExceptionHandler.java
@@ -1,16 +1,8 @@
 package com.ll.gramgram.base.exceptionHandler;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public String handleNotFoundError(Model model, NoHandlerFoundException ex) {
-        model.addAttribute("exception", ex);
-        return "error/404";
-    }
 }
