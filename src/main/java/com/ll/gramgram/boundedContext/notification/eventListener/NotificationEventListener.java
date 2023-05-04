61ad0e076288fdb854633e8f73973bfc74067197
daone <jcn07253@gmail.com>
2023-05-04 13:41:13 +0900

    호감표시가 발생한 후 그에따른 알림 메세지 생성

--- src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
@@ -2,6 +2,8 @@ package com.ll.gramgram.boundedContext.notification.eventListener;
 
 import com.ll.gramgram.base.event.EventAfterLike;
 import com.ll.gramgram.base.event.EventAfterModifyAttractiveType;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -13,15 +15,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Slf4j
 public class NotificationEventListener {
-    @EventListener
-    public void listen(EventAfterModifyAttractiveType event) {
-        // 누군가의 호감사유가 변경되었을 경우
-        log.debug("EventAfterModifyAttractiveType event : {}", event);
-    }
+    private final NotificationService notificationService;
 
     @EventListener
     public void listen(EventAfterLike event) {
-        // 누군가가 호감표시를 한 경우
-        log.debug("EventAfterLike event : {}", event);
+        LikeablePerson likeablePerson = event.getLikeablePerson();
+
+        notificationService.makeLike(likeablePerson);
+    }
+
+    @EventListener
+    public void listen(EventAfterModifyAttractiveType event) {
+
     }
 }
--- src/main/java/com/ll/gramgram/boundedContext/notification/service/NotificationService.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/service/NotificationService.java
@@ -1,19 +1,41 @@
 package com.ll.gramgram.boundedContext.notification.service;
 
+import com.ll.gramgram.base.rsData.RsData;
 import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import com.ll.gramgram.boundedContext.notification.entity.Notification;
 import com.ll.gramgram.boundedContext.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class NotificationService {
     private final NotificationRepository notificationRepository;
 
     public List<Notification> findByToInstaMember(InstaMember toInstaMember) {
         return notificationRepository.findByToInstaMember(toInstaMember);
+    }
+
+    @Transactional
+    public RsData<Notification> makeLike(LikeablePerson likeablePerson) {
+        Notification notification = Notification
+                .builder()
+                .typeCode("LIKE")
+                .toInstaMember(likeablePerson.getToInstaMember())
+                .fromInstaMember(likeablePerson.getFromInstaMember())
+                .oldAttractiveTypeCode(0)
+                .oldGender(null)
+                .newAttractiveTypeCode(likeablePerson.getAttractiveTypeCode())
+                .newGender(likeablePerson.getFromInstaMember().getGender())
+                .build();
+
+        notificationRepository.save(notification);
+
+        return RsData.of("S-1", "알림 메세지가 생성되었습니다.", notification);
     }
 }
