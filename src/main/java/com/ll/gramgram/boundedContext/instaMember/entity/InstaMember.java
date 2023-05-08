170428efb06d9af0ae60fb2772782ea03a5b8132
daone <jcn07253@gmail.com>
2023-05-08 10:22:05 +0900

    버그 수정

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/entity/InstaMember.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/entity/InstaMember.java
@@ -35,6 +35,7 @@ public class InstaMember extends InstaMemberBase {
 
     @OneToMany(mappedBy = "toInstaMember", cascade = {CascadeType.ALL})
     @OrderBy("id desc") // 정렬
+    @LazyCollection(LazyCollectionOption.EXTRA)
     @Builder.Default // @Builder 가 있으면 ` = new ArrayList<>();` 가 작동하지 않는다. 그래서 이걸 붙여야 한다.
 
     private List<LikeablePerson> toLikeablePeople = new ArrayList<>();
